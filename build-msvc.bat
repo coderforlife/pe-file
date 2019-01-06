8fdff8de572bf953591a671d3c65290c67d53f92
Jeffrey Bush <jeff@coderforlife.com>
2019-01-06 01:31:01 -0500

    Bug fix in new compile script.

--- build-msvc.bat
+++ build-msvc.bat
@@ -20,14 +20,14 @@
 @set FILES=PEFile.cpp PEFileResources.cpp PEDataSource.cpp PEVersion.cpp
 
 @echo Compiling 32-bit...
-@call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x86
+@call "%DIR%\..\..\VC\vcvarsall.bat" x86
 cl %FLAGS% /c %FILES%
 lib /nologo /ltcg /out:PEFile.lib *.obj
 @del /F /Q *.obj >NUL 2>&1
 @echo.
 
 @echo Compiling 64-bit...
-@call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x64
+@call "%DIR%\..\..\VC\vcvarsall.bat" x64
 cl %FLAGS% /c %FILES%
 lib /nologo /ltcg /out:PEFile64.lib *.obj
 @del /F /Q *.obj >NUL 2>&1
